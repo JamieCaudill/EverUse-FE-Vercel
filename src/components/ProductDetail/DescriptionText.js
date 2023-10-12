044cf05d8b9d9a1001f3905b9114bd1ae6b4f472
Rachel Prather <rachelprather1@gmail.com>
2023-10-11 17:16:17 -0700

    Remove redundant header

--- src/components/ProductDetail/DescriptionText.js
+++ src/components/ProductDetail/DescriptionText.js
@@ -3,7 +3,6 @@
 const DescriptionText = ({description}) => {
   return (
     <article className="product-description-text">
-      <h2>Description</h2>
       <p>
         {description}
       </p>
